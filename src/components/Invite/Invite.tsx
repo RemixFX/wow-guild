01a4349283903a330dc54b34cc94172208c4d282
RemixFX <remix.master@yandex.ru>
2023-03-30 13:29:53 +0300

    refactor: change the description of the Info component, discord-link

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -8,7 +8,7 @@ const Invite = () => {
       <p className="invite__text">
         Переходите в дискорд нашей гильдии и отправляйте заявку на вступление по форме.
       </p>
-      <a className="invite__link" target="_blank" rel="noreferrer" href="https://discord.gg/BTjPPfNAxy">
+      <a className="invite__link" target="_blank" rel="noreferrer" href="https://discord.gg/PJgHejJMnq">
         <span className="invite__span"></span>
         <span className="invite__span"></span>
         <span className="invite__span"></span>
@@ -16,7 +16,7 @@ const Invite = () => {
         <img className="invite__image" src={discordImage} alt="discord-logo" />
       </a>
       <div className="invite__widget">
-        <iframe src="https://discord.com/widget?id=891246510338084864" width="550" height="500" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <iframe src="https://discord.com/widget?id=681202141024485403" width="550" height="500" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </div>
     </section>
   )
